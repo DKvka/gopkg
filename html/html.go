6f7d5ea67ad227722e7635880a413c471d49f9c4
Deekay <118295931+DKvka@users.noreply.github.com>
2025-05-26 11:02:34 +0300

    Refactoring

---
+++ html/html.go
@@ -0,0 +1,8 @@
+package html
+
+// Link represents a link in an html document
+// <a href="...">Text</a>
+type Link struct {
+	Href string
+	Text string
+}
--- html/parse/parse.go
+++ html/parse/parse.go
@@ -4,29 +4,23 @@ import (
 	"fmt"
 	"io"
 
-	"golang.org/x/net/html"
+	"github.com/dkvka/gopkg/html"
+	xhtml "golang.org/x/net/html"
 )
-
-// Link represents a link in an html document
-// <a href="...">Text</a>
-type Link struct {
-	Href string
-	Text string
-}
 
 // TODO: Create this with only stdlib
 
 // parse.Links takes in an html document and returns a slice
 // of links parsed from it.
-func Links(r io.Reader) ([]Link, error) {
-	doc, err := html.Parse(r)
+func Links(r io.Reader) ([]html.Link, error) {
+	doc, err := xhtml.Parse(r)
 	if err != nil {
 		return nil, err
 	}
 
 	nodes := linkNodes(doc)
 
-	var links []Link
+	var links []html.Link
 
 	for _, node := range nodes {
 		links = append(links, makeLink(node))
@@ -36,11 +30,11 @@ func Links(r io.Reader) ([]Link, error) {
 	return links, nil
 }
 
-func linkNodes(n *html.Node) (nodes []*html.Node) {
-	if n.Type == html.ElementNode && n.Data == "a" {
-		return []*html.Node{n}
+func linkNodes(n *xhtml.Node) (nodes []*xhtml.Node) {
+	if n.Type == xhtml.ElementNode && n.Data == "a" {
+		return []*xhtml.Node{n}
 	}
-	
+
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
 		nodes = append(nodes, linkNodes(c)...)
 	}
@@ -48,7 +42,7 @@ func linkNodes(n *html.Node) (nodes []*html.Node) {
 	return
 }
 
-func makeLink(n *html.Node) (link Link) {
+func makeLink(n *xhtml.Node) (link html.Link) {
 	for _, attr := range n.Attr {
 		if attr.Key == "href" {
 			link.Href = attr.Val

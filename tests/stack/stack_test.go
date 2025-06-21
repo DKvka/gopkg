6ffe1617b800fd1351501699fdda80dec364d8ce
DKvka <diegokvka@gmail.com>
2025-06-21 17:45:52 +0300

    Queueing

--- tests/stack/stack_test.go
+++ tests/stack/stack_test.go
@@ -1,7 +1,10 @@
 package stack_test
 
-import "testing"
-import "github.com/dkvka/gopkg/dsa/stack"
+import (
+	"testing"
+
+	"github.com/dkvka/gopkg/dsa/stack"
+)
 
 func TestStack(t *testing.T) {
 	s := stack.New[uint8]()
@@ -11,13 +14,13 @@ func TestStack(t *testing.T) {
 		for i := uint8(0); i < PUSH_COUNT; i++ {
 			s.Push(i)
 		}
-		if s.Size() != int(PUSH_COUNT) {
+		if s.Size() != PUSH_COUNT {
 			t.Errorf("Expected stack size %d, got %d", PUSH_COUNT, s.Size())
-		} 
+		}
 
 		item, ok := s.Pop()
-		if !ok || item != 2 {
-			t.Errorf("Expected to pop 2, got %d, ok: %v", item, ok)
+		if !ok || item != 9 {
+			t.Errorf("Expected to pop 9, got %d, ok: %v", item, ok)
 		}
 
 		s.Clear()

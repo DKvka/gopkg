c85a4044b5ec048fc4f99e4b2f537c1d67feea81
DKvka <diegokvka@gmail.com>
2025-06-21 04:04:27 +0300

    Stacking

---
+++ tests/stack/stack_test.go
@@ -0,0 +1,28 @@
+package stack_test
+
+import "testing"
+import "github.com/dkvka/gopkg/dsa/stack"
+
+func TestStack(t *testing.T) {
+	s := stack.New[uint8]()
+
+	t.Run("Push, pop, clear", func(t *testing.T) {
+		const PUSH_COUNT = 10
+		for i := uint8(0); i < PUSH_COUNT; i++ {
+			s.Push(i)
+		}
+		if s.Size() != int(PUSH_COUNT) {
+			t.Errorf("Expected stack size %d, got %d", PUSH_COUNT, s.Size())
+		} 
+
+		item, ok := s.Pop()
+		if !ok || item != 2 {
+			t.Errorf("Expected to pop 2, got %d, ok: %v", item, ok)
+		}
+
+		s.Clear()
+		if len(s.items) != 0 {
+			t.Errorf("Expected empty stack after clear, got size %d", s.Size())
+		}
+	})
+}
